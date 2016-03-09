44d3582b46908666c5ad6d28feefdca9fdac0858
Carlos Renoj <renojpa@gmail.com>
2016-03-09 01:08:21 -0500

    initial commit

---
+++ index.ios.js
@@ -0,0 +1,16 @@
+var React = require('react-native');
+var {
+    Text,
+    View,
+    AppRegistry
+    } = React;
+
+var StopWatch = React.createClass({
+    render: function(){
+
+    }
+});
+
+
+
+AppRegistry.registerComponent('stopwatch', () => StopWatch);
