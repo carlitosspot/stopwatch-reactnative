d90f9d1f31ea785002d8f43cde8a5a85a7fc0279
Carlos Renoj <renojpa@gmail.com>
2016-03-10 23:43:08 -0500

    adds app layout

--- index.ios.js
+++ index.ios.js
@@ -2,15 +2,60 @@ var React = require('react-native');
 var {
     Text,
     View,
-    AppRegistry
+    AppRegistry,
+    StyleSheet
     } = React;
 
 var StopWatch = React.createClass({
     render: function(){
+        return <View>
+            <View>
+                <View>
+                    <Text>
+                        00:00.00
+                    </Text>
+                </View>
+                <View>
+                    {this.startStopButton()}
+                    {this.lapButton()}
+                </View>
+            </View>
 
+            <View>
+                <Text>
+                    I am list of laps
+                </Text>
+            </View>
+        </View>
+    },
+    startStopButton: function(){
+        return  <View>
+            <Text>
+                Start
+            </Text>
+        </View>
+    },
+    lapButton: function(){
+        return    <View>
+            <Text>
+                Lapt
+            </Text>
+        </View>
     }
 });
 
 
+var styles = StyleSheet.create({
+    container: {
+        flex: 1, // fill the entire screen
+        alignItems: 'stretch'
+    },
+    header:{ //yellow
+        flex: 1
+    },
+    footer:{ // blue
+        flex: 1
+    }
+});
 
 AppRegistry.registerComponent('stopwatch', () => StopWatch);
